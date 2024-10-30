8b80165075608eb69e9cbbc732a28cd61ca571a7
shivkumar swami <shivkumardeveloper@gmail.com>
2024-10-30 18:03:25 +0530

    added populate component.

---
+++ src/app/components/populate/populate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-populate',
+  templateUrl: './populate.component.html',
+  styleUrl: './populate.component.scss'
+})
+export class PopulateComponent {
+
+}
